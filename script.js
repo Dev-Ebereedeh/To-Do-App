5c8928ab6df370b1d991276ba24247f3a3adc67c
dev-ebereedeh <eberechukwgoodnesspharm@gmail.com>
2024-09-30 06:04:16 +0100

    upgraded functionality

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 let inputBox = document.getElementById("input-box");
 let listContainer = document.getElementById("list-container");
+let dueDateInput = document.getElementById("due-date")
+
+
 const addButton = document.getElementById("add-button");
+
+
+addButton.addEventListener("click", addTask);
+event.preventDefault();
 
 
 function addTask() {
     if (inputBox.value.trim() === "") {
         alert ("You must write something!");
+        return;
+
     }
+
+
     else{
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value <i class="fa-solid fa-trash"></i>;
+        li.innerHTML = `<span class="task-text">${inputBox.value}</span>  <i class='bx bx-trash' ></i>`;
         listContainer.appendChild(li);
-        // inputBox.value = "";
+        inputBox.value = "";
+        // dueDateInput.value = "";
 
-        addButton.addEventListener("click", addTask);
-
+       
 
         li.addEventListener("click", function addTask() {
             li.classList.toggle("completed");
         });
-
+        
+        
 
 
     }
